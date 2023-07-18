1badbef95b0fb0fe92cfdb77c287d5fc88f34125
minjong kim <alswhddkdl@gmail.com>
2023-07-18 20:57:49 +0900

    feat: 나머지 연산 추가

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,51 +5,52 @@ fun main() {
     while (true) {
         try {
             println("숫자 두 개를 띄어서 입력해주세요 ex)1 3 (숫자에 -1을 포함하면 종료됩니다.)")
-            println("가능한 연산은 덧셈, 뺄셈, 곱셈, 나눗셈, 나머지 구하기 입니다.")
             val br = System.`in`.bufferedReader()
             val (a, b) = br.readLine().split(" ").map { it.toDouble() }
+            var result = 0.0
+            lateinit var calc: Calculator
+
             if (a == -1.0 || b == -1.0)
                 break
-
-            val operator = br.readLine()
-            lateinit var calc: Calculator
-            var result = 0.0
+            println("어떤 연산할 할까요? 번호를 입력해주세요! 1)덧셈 2)뺄셈 3)곱셈 4)나눗셈")
+            val operator = br.readLine().toInt()
 
             when (operator) {
-                "+" -> {
+                1 -> {
                     val addOp: AbstractOperation = AddOperation()
                     calc = Calculator(addOp, a, b)
                     result = calc.operate()
                 }
 
-                "-" -> {
+                2 -> {
                     val subOp: AbstractOperation = SubtractOperation()
                     calc = Calculator(subOp, a, b)
                     result = calc.operate()
                 }
 
-                "*" -> {
+                3 -> {
                     val mulOp: AbstractOperation = MultiplyOperation()
                     calc = Calculator(mulOp, a, b)
                     result = calc.operate()
                 }
 
-                "/" -> {
+                4 -> {
                     val divOp: AbstractOperation = DivideOperation()
                     calc = Calculator(divOp, a, b)
                     result = calc.operate()
                 }
-
+                5 -> {
+                    val modularOp: AbstractOperation = ModularOperation()
+                    calc = Calculator(modularOp, a, b)
+                    result = calc.operate()
+                }
                 else -> {
                     println("잘못된 연산자입니다.")
                 }
             }
-
-            if (result.equals(0.0))
-                continue
-            else
-                println("$a $operator ${b}의 결과는 $result")
+            println("$a $operator ${b}의 결과는 $result")
         } catch (e: Exception) { // 좋은 방법이라 생각하진 않지만 일단 입력 오류 방지
+            println("잘못된 입력입니다.")
             continue
         }
     }
